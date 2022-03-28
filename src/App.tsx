1889db3a6a4855f4f28b7231e9d1b64a308b815b
amedina <steven@grydd.com>
2022-03-28 09:55:15 -0500

    Upload for deploy project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BikesList } from './containers/BikesList/BikesList';
+import { BikeDetail } from './components/BikeDetail/BikeDetail';
+import { Header } from './components/Header/Header';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<Header />
+			<div className='container'>
+				<Routes>
+					<Route path='/' element={<BikesList />} />
+					<Route path='/bike/:id' element={<BikeDetail />} />
+				</Routes>
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
